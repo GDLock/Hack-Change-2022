06e822465cd7c3381451a5f5d116b72a00174347
M4kkenzEe <chernigovtsev2001@mail.ru>
2022-11-12 19:20:00 +0300

    update-ui-5

--- app/src/main/java/com/otkritie/hackaton/screens/chatmenu/ChatMenuScreen.kt
+++ app/src/main/java/com/otkritie/hackaton/screens/chatmenu/ChatMenuScreen.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.otkritie.hackaton.R
 import com.otkritie.hackaton.databinding.FragmentChatMenuScreenBinding
@@ -40,7 +41,8 @@ class ChatMenuScreen : Fragment() {
 
         mBinding.apply {
 
-            //viewModel.requestEvent
+            val action = 
+
 
 
             questionAdapter.setList(lst)
